faa4762ef96fd783d62c0518442b9c2e7f6942c5
nnicha123 <nicha_ngamtweerat@hotmail.com>
2024-12-06 10:06:51 +0700

    Added error page, convert snippet create page to client to use useActionState to return error messages for form validation

--- snippets/src/actions/index.ts
+++ snippets/src/actions/index.ts
@@ -17,3 +17,47 @@ export async function deleteSnippet(id: number) {
   });
   redirect("/");
 }
+
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
+  try {
+    // Check user's inputs and make sure they're valid
+    const title = formData.get("title");
+    const code = formData.get("code");
+
+    if (typeof title !== "string" || title.length < 3) {
+      return {
+        message: "Title must be longer",
+      };
+    }
+
+    if (typeof code !== "string" || code.length < 10) {
+      return {
+        message: "Code must be longer",
+      };
+    }
+
+    // Create new record in database
+    await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        message: err.message,
+      };
+    } else {
+      return {
+        message: "Something went wrong...",
+      };
+    }
+  }
+
+  // Redirect the user back to root route
+  redirect("/");
+}
